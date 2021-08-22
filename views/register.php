6097d9a9bd20b1d46abba4dc01301ade1842eae7
Abdullayev Samandar <xkas2001@gmail.com>
2021-08-22 23:35:21 +0500

    Slight changes have been made.

--- views/register.php
+++ views/register.php
@@ -1,30 +1,18 @@
 <h1>Create an account</h1>
-<form action="" method="post">
-    <div class="row">
-        <div class="col">
-            <div class="mb-3">
-                <label class="form-label">Firstname</label>
-                <input type="text" name="firstname" class="form-control">
-            </div>
-        </div>
-        <div class="col">
-            <div class="mb-3">
-                <label class="form-label">Lastname</label>
-                <input type="email" name="lastname" class="form-control">
-            </div>
-        </div>
+<?php
+/**@var $model \app\core\Model*/
+?>
+<?php $form = \app\core\form\Form::begin('', "post") ?>
+<div class="row">
+    <div class="col">
+        <?= $form->field($model, 'firstname') ?>
     </div>
-    <div class="mb-3">
-        <label class="form-label">Email</label>
-        <input type="email" name="email" class="form-control">
+    <div class="col">
+        <?= $form->field($model, 'lastname') ?>
     </div>
-    <div class="mb-3">
-        <label class="form-label">Password</label>
-        <input type="password" name="password" class="form-control">
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Confirm Password</label>
-        <input type="password" name="confirmPassword" class="form-control">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+</div>
+<?= $form->field($model, 'email') ?>
+<?= $form->field($model, 'password')->passwordField() ?>
+<?= $form->field($model, 'passwordConfirm')->passwordField() ?>
+<button type="submit" class="btn btn-primary">Submit</button>
+<?php \app\core\form\Form::end(); ?>
